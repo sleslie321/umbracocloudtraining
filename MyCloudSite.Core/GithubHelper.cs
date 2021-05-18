2b3cebf331e31e793b21768ef599ce74e8a532da
Scott Leslie <scott@leslie.com>
2021-05-18 13:37:16 +0100

    custom code project

---
+++ MyCloudSite.Core/GithubHelper.cs
@@ -0,0 +1,15 @@
+﻿using Octokit;
+using System.Collections.Generic;
+
+namespace MyCloudSite.Core
+{
+    public class GithubHelper
+    {
+        public static IReadOnlyList<Repository> GetRepositories(string organisation)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("my-cool-app"));
+            return client.Repository.GetAllForOrg(organisation).Result;
+        }
+
+    }
+}
